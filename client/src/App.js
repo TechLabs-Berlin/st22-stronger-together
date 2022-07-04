fa5afeeb1da1deb4773583a21a822c838f7f08f9
tetianamakhlai <tetianamakhlai@gmail.com>
2022-07-04 22:37:31 +0200

    add rout

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
-//import RegisterPage from "./pages/RegisterPage";
+import RegisterPage from "./pages/RegisterPage";
 // TODO: Reimport once Material UI imports are updated
 // import ProfilePage from "./pages/ProfilePage";
 
 
-// <Route index element={<LandingPage />} />
-//<Route path="/login" element={<LoginPage />} />
-//<Route path="/register" element={<RegisterPage />} />
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<LoginPage />} />
+        <Route index element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
