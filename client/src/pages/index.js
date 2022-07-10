571165b35f91769eb4dbfc0ad265dced26ac5517
Jona-Bo <92577875+Jona-Bo@users.noreply.github.com>
2022-07-10 15:01:07 +0200

    Add files via upload

---
+++ client/src/pages/index.js
@@ -0,0 +1,10 @@
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    const modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    const items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
+  });
