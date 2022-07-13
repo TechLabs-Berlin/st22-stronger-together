17c7c14969c59150440aeb7ea80377e9b16d9607
Matheus Albuquerque <land@ythecombinator.space>
2022-07-13 20:15:36 +0200

    bootstrap profile info

--- client/src/App.js
+++ client/src/App.js
@@ -4,33 +4,13 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import RegisterPage from "./pages/RegisterPage";
 import FeedPage from "./pages/FeedPage";
-// imported from ReactFire
-import { doc, getFirestore } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import {
   FirestoreProvider,
   AuthProvider,
-  useFirestoreDocData,
-  useFirestore,
   useFirebaseApp,
 } from "reactfire";
-
-//Added a function component
-
-function BurritoTaste() {
-  // easily access the Firestore library
-  const burritoRef = doc(useFirestore(), "tryreactfire", "burrito");
-
-  // subscribe to a document for realtime updates. just one line!
-  const { status, data } = useFirestoreDocData(burritoRef);
-
-  // easily check the loading status
-  if (status === "loading") {
-    return <p>Fetching burrito flavor...</p>;
-  }
-
-  return <p>The burrito is {data.yummy ? "good" : "bad"}!</p>;
-}
 
 function App() {
   const app = useFirebaseApp();
--- client/src/components/ProfilePage/Main/Leftside/LeftSideTop.js
+++ client/src/components/ProfilePage/Main/Leftside/LeftSideTop.js
@@ -2,27 +2,44 @@ import React from "react";
 import Divider from "@mui/material/Divider";
 import "./LeftSideTop.css";
 
+// Firebase
+
+import { doc } from "firebase/firestore";
+import { useFirestoreDocData, useFirestore, useSigninCheck } from "reactfire";
+
 const LeftSideTop = () => {
+  // TODO: Use not hardcoded user
+  // const { data: signinResult } = useSigninCheck();
+  // const { user } = signinResult;
+
+  const userRef = doc(useFirestore(), "users", "U9XXGbFKgGMSTp1HRBbYUan7yBz1");
+  const { status, data } = useFirestoreDocData(userRef);
+
+  if (status === "loading") {
+    return <p>Fetching user information...</p>;
+  }
+
   return (
     <section className="leftTop">
       <div className="yourProfilemain">
         <h2 className="yourprofile">Your Profile</h2>
       </div>
-      <Divider className=" Divider MuiDivider-root MuiDivider-fullWidth css-9mgopn-MuiDivider-root"/>
+      <Divider className=" Divider MuiDivider-root MuiDivider-fullWidth css-9mgopn-MuiDivider-root" />
       <div className="profilePicture">
         <div>
-          <img className="profileimage"
+          <img
+            className="profileimage"
             src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
             alt="profile_pictre"
           ></img>
         </div>
         <div>
-          <h3>Name</h3>
-          <h4>Gender</h4>
+          <h3>{data.givenName}</h3>
+          <h4>{data.familyName}</h4>
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default LeftSideTop;
