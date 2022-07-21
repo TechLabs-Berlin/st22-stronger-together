9a4c700f22056867f7d4343e9db5508663c11835
BusayoAfolabi12 <97314339+BusayoAfolabi12@users.noreply.github.com>
2022-07-21 18:53:46 +0200

    update on hero section

--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -3,12 +3,18 @@ import Footer from "../components/Footer";
 import Header from "../components/Head/Header";
 import Hero from "../components/hero";
 import CallToAction from "../components/CallToAction";
+import './LandingPage.css';
 
 function LandingPage() {
   return (
     <div>
       <Header />
       <Hero />
+      <section>
+        <h4>
+        A platform to connect and help those in need.
+        </h4>
+      </section>
       <CardDetails />
       <CallToAction />
       <Footer />
