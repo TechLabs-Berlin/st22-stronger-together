f0c68dcc5cfc559fd239b02b5f20cdf033b76865
tetianamakhlai <tetianamakhlai@gmail.com>
2022-07-21 16:19:56 +0200

    LandingPage Call to Action

---
+++ client/src/components/CallToAction.js
@@ -0,0 +1,23 @@
+import React from "react";
+import SignUpIllustration from "../img/Group5798SignUp.png";
+import { Button } from "@mui/material";
+//import { useNavigate } from "react-router-dom";
+import "./CallToAction.css";
+
+const callToAction = () => {
+  //const navigate = useNavigate();
+  return (
+    <div className="container">
+      <img
+        className="SignUp"
+        src={SignUpIllustration}
+        width="50%"
+        height="100%"
+        alt="Illustration Sign Up"
+      />
+      <Button className="navButton signUp">Sign Up</Button>
+    </div>
+  );
+};
+
+export default callToAction;
--- client/src/pages/landingpage.js
+++ client/src/pages/landingpage.js
@@ -2,6 +2,7 @@ import Cards from "../components/Cards";
 import Footer from "../components/Footer";
 import Header from "../components/Head/Header";
 import Hero from "../components/hero";
+import CallToAction from "../components/CallToAction";
 
 function LandingPage() {
   return (
@@ -9,6 +10,7 @@ function LandingPage() {
       <Header />
       <Hero />
       <Cards />
+      <CallToAction />
       <Footer />
     </div>
   );
